d58e3931a44bccaa952225929237ae41fd88c583
JarlOfCode <jonasbt007@gmail.com>
2019-05-13 07:10:32 -0400

    Fixed dev_satellite marea css, removed dev_fcs as it's merged with dev_sat

--- dev_fcs/Demo/marea/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MareaConfig(AppConfig):
-    name = 'marea'
--- dev_fcs/Demo/parea/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PareaConfig(AppConfig):
-    name = 'parea'
